953520a81cda5d76131b9f9b3a9648f4c2e2cb95
mujibah <khadijahmujibahmohammad@gmail.com>
2024-12-22 11:49:14 +0100

    Delete khadijah directory

--- khadijah/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.flip-card').on('click touchend', function (event) {
-        event.preventDefault(); // Prevent any default behavior
-        $(this).find('.flip-card-inner').toggleClass('flipped');
-    });
-});
